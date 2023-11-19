4b273eb9032a69fd7fede6c25f2bcd353e5aa337
Miguel Alberto <2211444@my.ipleiria.pt>
2023-11-19 23:01:14 +0000

    TP-13 Allow Edit User Details

--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -15,27 +15,49 @@
           <img v-else alt="User photo" :src="photoUrl" />
         </ion-avatar>
         
+
         <div class="container" style="margin-top: 10%;">
-          <ion-list class="list-container">
-            <ion-item>
-              <ion-input label="Phone number" v-model="phoneNumber" label-placement="stacked" :readonly="true"></ion-input>
-            </ion-item>
-            <ion-item>
-              <ion-input label="Name" v-model="vcard.name" label-placement="stacked" :readonly="true"></ion-input>
-            </ion-item>
-            <ion-item>
-              <ion-input label="Email" v-model="vcard.email" label-placement="stacked" :readonly="true"></ion-input>
-            </ion-item>
-            <!-- <ion-item>
-              <ion-input label="Password" v-model="vcard.email" label-placement="stacked" :readonly="true"></ion-input>
-            </ion-item>
-            <ion-item>
-              <ion-input label="PIN" v-model="vcard.email" label-placement="stacked" :readonly="true"></ion-input>
-            </ion-item> -->
-          </ion-list>
-          <br>
-          <br>
-          <ion-button expand="block">Edit</ion-button>
+        <ion-list class="list-container" v-if="!isEditing">
+          <ion-item v-if="!isEditing">
+            <ion-input label="Phone number" v-model="phoneNumber" label-placement="stacked" :readonly="true"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input label="Name" v-model="vcard.name" label-placement="stacked" :readonly="true"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input label="Email" v-model="vcard.email" label-placement="stacked" :readonly="true"></ion-input>
+          </ion-item>
+          
+          <!-- <ion-item>
+            <ion-input label="Password" v-model="maskedPassword" label-placement="stacked" :readonly="true"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input label="PIN" v-model="maskedPIN" label-placement="stacked" :readonly="true"></ion-input>
+          </ion-item> -->
+
+        </ion-list>
+        <ion-list class="list-container" v-else>
+          <ion-item>
+            <ion-input label="Phone number" v-model="phoneNumber" label-placement="stacked" :readonly="true"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input label="Name" v-model="vcard.name" label-placement="stacked" :readonly="false"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input label="Email" v-model="vcard.email" label-placement="stacked" :readonly="false"></ion-input>
+          </ion-item>
+          <!-- <ion-item>
+            <ion-input label="Password" v-model="editablePassword" label-placement="stacked" type="password" :readonly="false"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input label="PIN" v-model="editablePIN" label-placement="stacked" type="password" :readonly="false"></ion-input>
+          </ion-item> -->
+        </ion-list>
+        <br>
+        <br>
+        <ion-button expand="block" @click="toggleEditMode">{{ isEditing ? 'Save' : 'Edit' }}</ion-button>
+        <!-- <ion-button expand="block" @click="toggleEditMode">{{ isEditing ? 'Save' : 'Change Password and PIN' }}</ion-button> -->
+        
         </div>
       </ion-content>
     </ion-page>
@@ -77,4 +99,41 @@ onMounted(() => {
       console.log(error)
     })
 });
+
+const maskedPassword = "********"; //computed(() => '*'.repeat(8));
+const maskedPIN = "***"; //computed(() => '*'.repeat(3));
+
+const editablePassword = ref('');
+const editablePIN = ref('');
+const isEditing = ref(false);
+
+const toggleEditMode = async () => {
+  if (isEditing.value) {
+    // Save changes
+    try {
+      await axios.put(`vcard/${props.phone}`, {
+        name: vcard.value.name,
+        email: vcard.value.email,
+        //password: editablePassword.value,
+        //pin: editablePIN.value,
+      });
+
+      // Optionally update the local vcard with the edited values
+      vcard.value.name = vcard.value.name;
+      vcard.value.email = vcard.value.email;
+      //vcard.value.password = editablePassword.value;
+      //vcard.value.pin = editablePIN.value;
+
+      // Perform any additional actions after successful save
+    } catch (error) {
+      console.error('Error saving changes:', error);
+    }
+  } else {
+    // Enter edit mode
+    //editablePassword.value = vcard.value.password;
+    //editablePIN.value = vcard.value.pin;
+  }
+
+  isEditing.value = !isEditing.value;
+};
 </script>
