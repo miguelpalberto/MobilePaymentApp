63866ad071d27c784d116c4e548dd2b8468c38c4
João Silva <2211429@my.ipleiria.pt>
2023-11-20 01:09:55 +0000

    TP-28 Add main.css reference in main.ts

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,8 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import './assets/css/main.css';
+
 import axios from 'axios';
 const serverBaseUrl = config.serverBaseUrl
 
