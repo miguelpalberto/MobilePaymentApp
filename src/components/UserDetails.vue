fa3856742c81acf35a938644e1aff9b39905513f
João Silva <2211429@my.ipleiria.pt>
2023-12-11 01:23:42 +0000

    TP-11 Add delete vCard feature in UserDetails.vue

--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -21,9 +21,13 @@
       </ion-avatar>
 
 
-      <div class="container" style="margin-top: 10%;">
-        <ion-list class="list-container" v-if="!isEditing">
-          <ion-item v-if="!isEditing">
+      <div class="container" style="display:flex; height: 93%; 
+    flex-flow: column wrap;
+    justify-content: space-between;">
+        <div>
+
+          <ion-list class="list-container" v-if="!isEditing">
+            <ion-item v-if="!isEditing">
             <ion-input label="Phone number" v-model="phoneNumber" label-placement="stacked" :readonly="true"></ion-input>
           </ion-item>
           <ion-item>
@@ -39,7 +43,7 @@
           <ion-item>
             <ion-input label="PIN" v-model="maskedPIN" label-placement="stacked" :readonly="true"></ion-input>
           </ion-item> -->
-
+          
         </ion-list>
         <ion-list class="list-container" v-else>
           <ion-item>
@@ -58,13 +62,24 @@
             <ion-input label="PIN" v-model="editablePIN" label-placement="stacked" type="password" :readonly="false"></ion-input>
           </ion-item> -->
         </ion-list>
-        <br>
-        <br>
-        <ion-button expand="block" :color="isEditing ? 'secondary' : 'primary'" @click="toggleEditMode"> {{ isEditing ?
-          'Save' : 'Edit' }}</ion-button>
-        <ion-button expand="block" color="danger" @click="dismissVCard" :disabled="isEditing"> Dismiss </ion-button>
+        
+        <ion-button expand="block" :color="isEditing ? 'secondary' : 'primary'" @click="toggleEditMode">
+          {{ isEditing ? 'Save' : 'Edit' }}
+        </ion-button>
+      </div>
+        
         <!-- <ion-button expand="block" @click="toggleEditMode">{{ isEditing ? 'Save' : 'Change Password and PIN' }}</ion-button> -->
-
+        
+        <div>
+          <ion-button expand="block" color="warning" @click="dismissVCard" :disabled="isEditing">
+            <ion-icon :icon="personRemoveOutline"></ion-icon>
+            &nbsp;Dismiss
+          </ion-button>
+          <ion-button expand="block" color="danger" @click="deleteVCard" :disabled="isEditing">
+            <ion-icon :icon="trash"></ion-icon>
+            &nbsp;Delete
+          </ion-button>
+        </div>
       </div>
 
 
@@ -107,6 +122,40 @@
           <ion-button expand="full" color="success" @click="closeModals">No</ion-button>
         </ion-content>
       </ion-modal>
+
+      <ion-modal v-if="showDeleteVCardConfirmation" :is-open="showDeleteVCardConfirmation">
+        <ion-content class="ion-text-center">
+          <ion-card>
+            <ion-card-content>
+              <ion-card-title>Delete VCard</ion-card-title>
+              <p>Are you sure you want to delete this VCard?</p>
+              <ion-item>
+                <ion-label position="floating">Password</ion-label>
+                <ion-input v-model="confirmationPassword" type="password"
+                  @ion-input="validateDeleteRequestData"></ion-input>
+                <ion-text v-if="deleteFormErrors && deleteFormErrors.password" color="danger">
+                  {{ deleteFormErrors.password[0] }}
+                </ion-text>
+              </ion-item>
+              <ion-item>
+                <ion-label position="floating">PIN</ion-label>
+                <ion-input v-model="confirmationPin" type="password" @ion-input="validateDeleteRequestData"></ion-input>
+                <ion-text v-if="deleteFormErrors && deleteFormErrors.confirmation_code" color="danger">
+                  {{ deleteFormErrors.confirmation_code[0] }}
+                </ion-text>
+              </ion-item>
+              <ion-item>
+                <ion-text style="margin-top:30px" v-if="deleteFormErrors && deleteFormErrors.balance" color="danger">
+                  {{ deleteFormErrors.balance[0] }}
+                </ion-text>
+              </ion-item>
+            </ion-card-content>
+          </ion-card>
+          <ion-button expand="full" color="danger" :disabled="isLoading || !isDeleteFormValid"
+            @click="deleteVCardConfirmed">Yes</ion-button>
+          <ion-button expand="full" color="success" @click="closeModals">No</ion-button>
+        </ion-content>
+      </ion-modal>
     </ion-content>
   </ion-page>
 </template>
@@ -116,10 +165,12 @@ import { ref, inject, onMounted } from 'vue';
 import {
   IonPage, IonContent, IonList, IonItem, IonInput, IonButton,
   IonAvatar, IonSpinner, IonButtons, IonBackButton, IonModal,
-  IonCard, IonCardContent, IonCardTitle, IonHeader, IonToolbar, IonTitle
+  IonCard, IonCardContent, IonCardTitle, IonHeader, IonToolbar,
+  IonTitle, IonText, IonLabel, IonIcon
 } from '@ionic/vue';
 import { Storage } from '@ionic/storage';
 import { useRouter } from 'vue-router'
+import { trash, personRemoveOutline } from 'ionicons/icons';
 
 const axios = inject('axios');
 
@@ -174,6 +225,12 @@ const isEditing = ref(false);
 const showSuccessModal = ref(false);
 const showErrorModal = ref(false);
 const showDismissVCardConfirmation = ref(false);
+const showDeleteVCardConfirmation = ref(false);
+
+const confirmationPin = ref('');
+const confirmationPassword = ref('');
+const deleteFormErrors = ref(null);
+const isDeleteFormValid = ref(false);
 
 
 const dismissVCard = async () => {
@@ -187,6 +244,56 @@ const dismissVCardConfirmed = async () => {
   closeModals();
   router.push('/login');
 };
+
+const deleteVCard = async () => {
+  showDeleteVCardConfirmation.value = true;
+  validateDeleteRequestData();
+};
+
+const deleteVCardConfirmed = async () => {
+  try {
+    await axios.delete(`vcards/${props.phone}/delete`, {
+      data: {
+        password: confirmationPassword.value,
+        confirmation_code: confirmationPin.value
+      }
+    }).then(async (response) => {
+      await store.remove('token');
+      await store.remove('phone_number');
+      await store.remove('pin');
+      closeModals();
+      router.push('/login');
+    })
+  }
+  catch (error) {
+    if (error.response.status == 422) {
+      deleteFormErrors.value = error.response.data.errors;
+    }
+
+    console.log(error);
+  }
+};
+
+const validateDeleteRequestData = () => {
+  deleteFormErrors.value = {
+    confirmation_code: [],
+    password: [],
+    balance: []
+  };
+
+  let isValid = true;
+  if (confirmationPassword.value == '') {
+    deleteFormErrors.value.password.push('The password field is empty');
+    isValid = false;
+  }
+
+  if (confirmationPin.value == '') {
+    deleteFormErrors.value.confirmation_code.push('The confirmation code field is empty');
+    isValid = false;
+  }
+
+  isDeleteFormValid.value = isValid;
+}
 
 const toggleEditMode = async () => {
   if (isEditing.value) {
@@ -227,9 +334,15 @@ const toggleEditMode = async () => {
   isEditing.value = !isEditing.value;
 };
 
+
+
 const closeModals = () => {
   showSuccessModal.value = false;
   showErrorModal.value = false;
   showDismissVCardConfirmation.value = false;
+  showDeleteVCardConfirmation.value = false;
+  confirmationPassword.value = '';
+  confirmationPin.value = '';
+  deleteFormErrors.value = null;
 };
 </script>
