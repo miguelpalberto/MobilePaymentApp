f18f51811838742cb1bb2f45fc6d70a54dc236fe
João Silva <2211429@my.ipleiria.pt>
2023-12-11 01:11:18 +0000

    TP-11 Merge with TP-77

---
+++ ios/App/App/public/assets/status-tap-d5e7ac09.js
@@ -0,0 +1,3 @@
+import{au as r,av as i,aw as c,ax as d,ay as l}from"./index-4698ed6d.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
